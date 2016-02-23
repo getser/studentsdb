c10989aeaf6518b610e76b1c35e7373e76495c6a
Sergii Getmanchuk <getser79@gmail.com>
2016-02-23 22:06:15 +0200

    !!db.py!!, finished groups.py and groups_list.html

--- students/views/groups.py
+++ students/views/groups.py
@@ -2,28 +2,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from ..models import Group
 
 
 # Views for groups
 
 def groups_list(request):
-    groups = (
-        {'id':1,
-         'leader': {'id': 1, 'name': u'Гетьманчук'},
-         'title': 'Мтм-223',},
-        {'id':2,
-         'leader': {'id': 2, 'name': u'Іванов'},
-         'title': 'Мтм-221',},
-        {'id':3,
-         'leader': {'id': 3, 'name': u'Петров'},
-         'title': 'Мтм-232',},
-        {'id':4,
-         'leader': {'id': 4, 'name': u'Дрозд'},
-         'title': 'Мтм-234',},
-        )
+
+    groups = Group.objects.all()
+
+    order_by = request.GET.get('order_by', '')
+
+    if order_by in ('id', 'title', 'leader'):
+        groups = groups.order_by(order_by)
+
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page', '')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        groups = paginator.page(1)
+    except EmptyPage:
+        groups = paginator.page(paginator.num_pages)
+
+    # groups = (
+    #     {'id':1,
+    #      'leader': {'id': 1, 'second_name': u'Гетьманчук'},
+    #      'title': 'Мтм-223',},
+    #     {'id':2,
+    #      'leader': {'id': 2, 'second_name': u'Іванов'},
+    #      'title': 'Мтм-221',},
+    #     {'id':3,
+    #      'leader': {'id': 3, 'second_name': u'Петров'},
+    #      'title': 'Мтм-232',},
+    #     {'id':4,
+    #      'leader': {'id': 4, 'second_name': u'Дрозд'},
+    #      'title': 'Мтм-234',},
+    #     )
 
     return render(request, 'students/groups_list.html', {'groups':groups})
-    # return HttpResponse('<h1>Groups Listing</h1>')
 
 
 def groups_add(request):
--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -14,6 +14,9 @@ import os
 
 from django.conf import global_settings
 
+from . import db
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -84,12 +87,17 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
-    }
-}
+DATABASES = db.DATABASES
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'HOST': 'localhost',
+#         'USER': 'students_db_user',
+#         'PASSWORD': '******',
+#         'NAME': 'students_db',
+#     }
+# }
 
 
 # Password validation
