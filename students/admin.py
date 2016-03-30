e1cffc457535153afc0d0068fa9211cda5aa9c5e
Sergii Getmanchuk <getser79@gmail.com>
2016-03-30 18:26:11 +0300

    castomized groups admin form

--- students/admin.py
+++ students/admin.py
@@ -17,7 +17,7 @@ class StudentFormAdmin(ModelForm):
     """docstring for StudentFormAdmin"""
     
     def  clean_student_group(self):
-        """Check if student is lesder in any group.
+        """Check if student is leader in any group.
         if yes, then ensure it's the same as selected group."""
         # get group where current student is a leader
         groups = Group.objects.filter(leader=self.instance)
@@ -25,19 +25,6 @@ class StudentFormAdmin(ModelForm):
             raise ValidationError(u'Студент є старостою іншої групи.', code='invalid')
 
         return self.cleaned_data['student_group']
-
-
-
-# class StudentFormAdmin(ModelForm):
-#     def clean_student_group(self):
-#         """Check if student is leader in any group.
-#         If yes, then ensure it's the same as selected group."""
-#         # get group where current student is a leader
-#         groups = Group.objects.filter(leader=self.instance)
-#         if len(groups) > 0 and self.cleaned_data['student_group'] != groups[0]:
-#             raise ValidationError(u'Студент є старостою іншої групи.', code='invalid')
-#         return self.cleaned_data['student_group']
-
         
 
 class StudentAdmin(admin.ModelAdmin):
@@ -56,11 +43,24 @@ class StudentAdmin(admin.ModelAdmin):
         return reverse('students_edit', kwargs={'pk':obj.id})
 
 
+class GroupAdmin(admin.ModelAdmin):
+    
+    list_display = ['title', 'leader', 'notes']
+    list_display_links = ['title']
+    list_editable = ['leader']
+    ordering = ['title']
+    list_filter = ['leader']
+    list_per_page = 7
+    search_fields = ['title', 'leader']
+
+    # def view_on_site(self, obj):    
+        # return reverse('')
+
 
 # Register your models here.
 
 admin.site.register(Student, StudentAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
 
 admin.site.register(Exam)
 admin.site.register(Teacher)
